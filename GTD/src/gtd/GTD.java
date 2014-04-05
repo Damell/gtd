3ceb9f6b6df36c42dbf117d6d76d74aa031ea1dd
Daniel Chabr <chabrdan@fit.cvut.cz>
2014-04-05 14:39:47 +0200

    GUI design prototype

---
+++ GTD/src/gtd/GTD.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package gtd;
+
+import gtd.pl.GtdGUI;
+
+/**
+ *
+ * @author damell
+ */
+public class GTD {
+
+	private static GtdGUI  gui;
+
+	public static void main(String[] args) {
+		gui = new GtdGUI();
+		gui.setVisible(true);
+	}
+	
+}

fee0ca8f4134e95d991cfd624d3f7ec80fd6626a
centos <centos@localhost.localdomain>
2014-05-09 22:07:04 +0200

    getAllOsoby implementovano

--- GTD/src/GTD/DL/DLDAO/DAOOsoba.java
+++ GTD/src/GTD/DL/DLDAO/DAOOsoba.java
@@ -18,105 +18,119 @@ import java.util.List;
  */
 public class DAOOsoba implements IDAOOsoba {
 
+    public DAOOsoba() {
 
-	public DAOOsoba() {
+    }
 
-	}
+    /**
+     * Vytvoří nového uživatele.
+     *
+     * @param osoba
+     */
+    public boolean createOsoba(Osoba osoba) {
+        return false;
+    }
 
-	/**
-	 * Vytvoří nového uživatele.
-	 *
-	 * @param osoba
-	 */
-	public boolean createOsoba(Osoba osoba) {
-		return false;
-	}
+    /**
+     * Deaktivuje uživatele (na jeho účet se nepůjde přihlásit).
+     *
+     * @param osoba
+     */
+    public boolean deactivateOsoba(Osoba osoba) {
+        return false;
+    }
 
-	/**
-	 * Deaktivuje uživatele (na jeho účet se nepůjde přihlásit).
-	 *
-	 * @param osoba
-	 */
-	public boolean deactivateOsoba(Osoba osoba) {
-		return false;
-	}
+    /**
+     * Vrátí všechny osoby.
+     */
+    public List<Osoba> getAllOsoby() {
+        List<Osoba> osoby = new ArrayList<Osoba>();
+        Connection con = DatabaseConnection.getConnection();
+        try {
+            Statement stmt = con.createStatement();
+            //Podminka pro prihlasenou osobu + DatabaseConnection.getID());
+            ResultSet rset = stmt.executeQuery("select id, login, fname, sname from pavlim33.persons_v");
+            while (rset.next()) {
+                Osoba oso = new Osoba(rset.getInt(1), rset.getString(2), rset.getString(3), rset.getString(4));
+                osoby.add(oso);
+            }
+            rset.close();
+            stmt.close();
+        } catch (SQLException e) {
+            System.err.println("DB query error: " + e.getMessage());
+        }
+        return osoby;
+    }
 
-	/**
-	 * Vrátí všechny osoby.
-	 */
-	public List<Osoba> getAllOsoby() {
-		return new ArrayList<>();
-	}
+    /**
+     * Vrátí osobu podle jejího ID.
+     *
+     * @param id
+     */
+    public Osoba getOsoba(int id) {
+        Osoba osoba = null;
+        Connection con = DatabaseConnection.getConnection();
+        try {
+            Statement stmt = con.createStatement();
+            ResultSet rset = stmt.executeQuery("select id_person, login, fname, sname from pavlim33.members_v where id_person =" + id);
+            while (rset.next()) {
+                osoba = new Osoba(rset.getInt(1), rset.getString(2), rset.getString(3), rset.getString(4));
+            }
+            rset.close();
+            stmt.close();
+        } catch (SQLException e) {
+            System.err.println("DB query error");
+        }
+        return osoba;
+    }
 
-	/**
-	 * Vrátí osobu podle jejího ID.
-	 *
-	 * @param id
-	 */
-	public Osoba getOsoba(int id) {
-		Osoba osoba = null;
-		Connection con = DatabaseConnection.getConnection();
-		try {
-			Statement stmt = con.createStatement();
-			ResultSet rset = stmt.executeQuery("select id_person, login, fname, sname from pavlim33.members_v where id_person =" + id);
-			while (rset.next()) {
-				osoba = new Osoba(rset.getInt(1), rset.getString(2), rset.getString(3), rset.getString(4));
-			}
-			rset.close();
-			stmt.close();
-		} catch (SQLException e) {
-			System.err.println("DB query error");
-		}
-		return osoba;
-	}
+    /**
+     * Uloží změny osoby.
+     *
+     * @param osoba
+     */
+    public boolean updateOsoba(Osoba osoba) {
+        return false;
+    }
 
-	/**
-	 * Uloží změny osoby.
-	 *
-	 * @param osoba
-	 */
-	public boolean updateOsoba(Osoba osoba) {
-		return false;
-	}
+    /**
+     * Zkontroluje, jestli už neexistuje uživatel s daným uživ. jménem.
+     *
+     * @param login
+     */
+    public boolean checkNewLogin(String login) {
+        return false;
+    }
 
-	/**
-	 * Zkontroluje, jestli už neexistuje uživatel s daným uživ. jménem.
-	 *
-	 * @param login
-	 */
-	public boolean checkNewLogin(String login) {
-		return false;
-	}
+    /**
+     * Zkontroluje přihlašovací údaje.
+     *
+     * @param login
+     * @param heslo
+     */
+    public boolean checkPrihlaseni(String login, String heslo) {
+        boolean authenticated = false;
+        Connection con = DatabaseConnection.getConnection();
+        try {
+            Statement stmt = con.createStatement();
+            ResultSet rset = stmt
+                    .executeQuery("select login, pass_hash from persons");
+            while (rset.next()) {
+                if (rset.getString(1).equals(login) && String.valueOf(rset.getLong(2)).equals(heslo)) {
+                    authenticated = true;
+                }
+            }
+            rset.close();
+            stmt.close();
+        } catch (SQLException e) {
+            System.err.println("DB query error");
+        }
+        return authenticated;
+    }
 
-	/**
-	 * Zkontroluje přihlašovací údaje.
-	 *
-	 * @param login
-	 * @param heslo
-	 */
-	public boolean checkPrihlaseni(String login, String heslo) {
-		boolean authenticated = false;
-		Connection con = DatabaseConnection.getConnection();
-		try {
-			Statement stmt = con.createStatement();
-			ResultSet rset = stmt
-				.executeQuery("select login, pass_hash from persons");
-			while (rset.next()) {
-				if (rset.getString(1).equals(login) && String.valueOf(rset.getLong(2)).equals(heslo)) {
-					authenticated = true;
-				}
-			}
-			rset.close();
-			stmt.close();
-		} catch (SQLException e) {
-			System.err.println("DB query error");
-		}
-		return authenticated;
-	}
-
-	@Override
-	public Osoba getOsoba(String username) {
-		return null;
-	}
+    @Override
+    public Osoba getOsoba(String username) {
+        return null;
+    }
 
 }

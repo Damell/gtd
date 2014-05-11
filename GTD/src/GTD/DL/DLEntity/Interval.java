e45a3b56c88d06758cdc07ffaa4183a297f152ef
centos <centos@localhost.localdomain>
2014-05-11 16:45:57 +0200

    Internal - format kodu

--- GTD/src/GTD/DL/DLEntity/Interval.java
+++ GTD/src/GTD/DL/DLEntity/Interval.java
@@ -4,47 +4,48 @@ import java.util.Date;
 
 /**
  * Trída predstavuje casový interval pro daný úkol.
+ *
  * @author andel
  * @version 1.0
  * @created 26-4-2014 14:51:22
  */
 public class Interval {
-	
-	/**
-	 * Casový pocátek úkolu. Minimální presnost jsou dny.
-	 */
-	private Date from;
-	/**
-	 * Casový konec úkolu. Minimální presnost jsou dny.
-	 */
-	private Date to;
 
-	/*
-	* Nastav interval
-	*/
-	public void setInterval (Date from,Date to){
-		this.from=from;
-		this.to=to;
-	}
+    /**
+     * Casový pocátek úkolu. Minimální presnost jsou dny.
+     */
+    private Date from;
+    /**
+     * Casový konec úkolu. Minimální presnost jsou dny.
+     */
+    private Date to;
 
-	public Date getFrom() {
-		return from;
-	}
+    /*
+     * Nastav interval
+     */
+    public void setInterval(Date from, Date to) {
+        this.from = from;
+        this.to = to;
+    }
 
-	public Date getTo() {
-		return to;
-	}
+    public Date getFrom() {
+        return from;
+    }
 
-	public boolean isSet () {
-		return from != null && to != null;
-	}
+    public Date getTo() {
+        return to;
+    }
 
-	public Interval(){
-	}
+    public boolean isSet() {
+        return from != null && to != null;
+    }
 
-	public Interval(Date from, Date to){
-		this.from = from;
-		this.to = to;
-	}
-	
+    public Interval() {
+    }
+
+    public Interval(Date from, Date to) {
+        this.from = from;
+        this.to = to;
+    }
+
 }

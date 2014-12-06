881b01f3d5a1001101daa104426e246e334ca3ca
Šimon Steklík <steklsim@fit.cvut.cz>
2014-12-06 18:14:46 +0100

    cleaning up

--- GTD/src/main/java/GTD/restapi/TaskRestController.java
+++ GTD/src/main/java/GTD/restapi/TaskRestController.java
@@ -10,14 +10,10 @@ import GTD.BL.BLAktivity.ProjectAdmin;
 import GTD.BL.BLAktivity.TaskAdmin;
 import GTD.BL.BLOsoby.PersonAdmin;
 import GTD.DL.DLDAO.DAOActivity;
-import GTD.DL.DLDAO.DAOException;
 import GTD.DL.DLDAO.DAOPerson;
 import GTD.DL.DLDAO.DAOProject;
 import GTD.DL.DLDAO.DAOState;
 import GTD.DL.DLDAO.DAOTask;
-import GTD.DL.DLDAO.ItemNotFoundException;
-import GTD.DL.DLEntity.Context;
-import GTD.DL.DLEntity.Interval;
 import GTD.DL.DLEntity.Person;
 import GTD.DL.DLEntity.Project;
 import GTD.DL.DLEntity.Task;
@@ -25,29 +21,15 @@ import GTD.DL.DLEntity.TaskState;
 import GTD.DL.DLInterfaces.IDAOActivity;
 import GTD.DL.DLInterfaces.IDAOPerson;
 import GTD.DL.DLInterfaces.IDAOProject;
-import GTD.DL.DLInterfaces.IDAOState;
 import GTD.DL.DLInterfaces.IDAOTask;
-import GTD.DL.hibernate.HibernateUtil;
 
-import java.io.StringReader;
-import java.util.Date;
 import java.util.List;
-import javax.json.Json;
-import javax.json.JsonArrayBuilder;
-import javax.json.JsonException;
-import javax.json.JsonObject;
-import javax.json.JsonObjectBuilder;
-import javax.json.JsonReader;
-import javax.json.stream.JsonParser;
 
-import org.hibernate.exception.ConstraintViolationException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -64,12 +46,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/v1/tasks")
 public class TaskRestController {
-
-//	public static final String JSON_TASK_TITLE = "title";
-//	public static final String JSON_TASK_DESCRIPTION = "description";
-//	public static final String JSON_TASK_CREATOR = "creator";
-//	// etc... 
-//	// TODO steklsim ^ use these (or other) constants instead of strings
 
   private TaskAdmin taskAdmin;
   private PersonAdmin personAdmin;
@@ -91,7 +67,7 @@ public class TaskRestController {
 
   public TaskRestController() {
     IDAOActivity da = new DAOActivity();
-    IDAOTask dt = new DAOTask();
+	IDAOTask dt = new DAOTask();
     IDAOPerson dp = new DAOPerson();
     IDAOProject dpr = new DAOProject();
 
@@ -109,17 +85,6 @@ public class TaskRestController {
   Task get(@PathVariable int id) {
     // TODO steklsim add authentication
     DAOTask daoTask = new DAOTask(); // TODO steklsim az bude autentizace bude se nacitat pres taskAdmin
-
-//		HttpHeaders httpHeaders = new HttpHeaders();
-//		httpHeaders.setContentType(MediaType.APPLICATION_JSON);
-//		
-//		try {
-//			Task t = daoTask.get(id);
-//			HttpStatus status = t != null ? HttpStatus.OK : HttpStatus.NOT_FOUND;
-//			return new ResponseEntity<>(getJSONFromTask(t).toString(), httpHeaders, status);
-//		} catch (DAOException e) {
-//			return new ResponseEntity<>(null, httpHeaders, HttpStatus.INTERNAL_SERVER_ERROR);
-//		}
     Task task = daoTask.get(id);
 //		if (task == null) throw new ItemNotFoundException("Task with id '" + id + "' not found");
     return task;
@@ -128,17 +93,10 @@ public class TaskRestController {
   @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
   public ResponseEntity<?> delete(@PathVariable int id) {
     DAOTask dt = new DAOTask();
-//		dt.setSessionFactory(HibernateUtil.getSessionFactory());
-
-//		try {
     Task t = dt.get(id);
     if (t != null) dt.delete(t);
 //			HttpStatus status = t != null ? HttpStatus.OK : HttpStatus.NOT_FOUND;
     return new ResponseEntity<>(null, null, HttpStatus.OK);
-
-//		} catch (DAOException e) {
-//			return new ResponseEntity<>(null, null, HttpStatus.INTERNAL_SERVER_ERROR);
-//		}
   }
 
   @RequestMapping(method = RequestMethod.GET)
@@ -147,22 +105,6 @@ public class TaskRestController {
   List<Task> getAll() {
     logger.debug(getMessageSource().getMessage("restApi.taskCont.tasks.get.request.accepted", null, null));
     DAOTask dt = new DAOTask();
-//		dt.setSessionFactory(HibernateUtil.getSessionFactory());
-
-//		HttpHeaders httpHeaders = new HttpHeaders();
-//		httpHeaders.setContentType(MediaType.APPLICATION_JSON);
-//		try {
-//			List<Task> tasks = taskAdmin.getAllUkoly();
-//
-//			JsonArrayBuilder builder = Json.createArrayBuilder();
-//			for (Task t : tasks) {
-//				JsonObject obj = getJSONFromTask(t);
-//				builder.add(obj);
-//			}
-//			return new ResponseEntity<>(builder.build().toString(), httpHeaders, HttpStatus.OK);
-//		} catch (DAOException e) {
-//			return new ResponseEntity<>(null, httpHeaders, HttpStatus.INTERNAL_SERVER_ERROR);
-//		}
 
     return dt.getAll();
   }
@@ -170,32 +112,12 @@ public class TaskRestController {
   @RequestMapping(method = RequestMethod.POST)
   @ResponseStatus(HttpStatus.CREATED)
   public Task create(@RequestBody Task task) {
-//		try {
-//			JsonObject json = getJsonObjectFromString(taskString);
-//			
-//			Task task = getTaskFromJSON(json);
     Person testUser = personAdmin.getOsoba(ApiConstants.TEST_USER_ID);
 //			if (testUser == null) throw new ItemNotFoundException("User with id '" + ApiConstants.TEST_USER_ID + "' not found");
     populateTask(task, testUser);
 
     taskAdmin.addUkol(task, testUser, null); // TODO steklsim what about task's activity?
 
-//			JsonObject taskJson = getJSONFromTask(task);
-//			
-//			HttpHeaders httpHeaders = new HttpHeaders();
-//			httpHeaders.setContentType(MediaType.APPLICATION_JSON);
-//			
-//			return new ResponseEntity<>(taskJson.toString(), httpHeaders, HttpStatus.CREATED);
-//		
-//		} catch (DAOException de) {
-//			if (de.getCause() instanceof ConstraintViolationException) { // TODO steklsim is this enough?
-//				return new ResponseEntity<>(null, null, HttpStatus.BAD_REQUEST);
-//			} else {
-//				return new ResponseEntity<>(null, null, HttpStatus.INTERNAL_SERVER_ERROR);
-//			}
-//		} catch (JsonException je) {
-//			return new ResponseEntity<>(null, null, HttpStatus.BAD_REQUEST);
-//		}
     return task;
   }
 
@@ -203,35 +125,7 @@ public class TaskRestController {
   public
   @ResponseBody
   Task update(@PathVariable int id, @RequestBody Task task) {
-//		try {
-//			JsonObject json = getJsonObjectFromString(taskString);
-//			
-//			Person creator = personAdmin.getOsoba(json.getInt("creator"));
-////			if (creator == null) return new ResponseEntity<>(null, null, HttpStatus.BAD_REQUEST);
-//			if (creator == null) throw new Item;
-//			
-//			Task dbTask = taskAdmin.getUkol(id, creator); // TODO steklsim change this when authentization is working
-//			if (dbTask == null) return new ResponseEntity<>(null, null, HttpStatus.BAD_REQUEST);
-//			Task task = getTaskFromJSON(json, dbTask);
-//			
-//			taskAdmin.updateUkol(task, task.getCreator());
-//			Task updatedTask = taskAdmin.getUkol(id, creator);
-//			JsonObject taskJson = getJSONFromTask(updatedTask);
-//			
-//			HttpHeaders httpHeaders = new HttpHeaders();
-//			httpHeaders.setContentType(MediaType.APPLICATION_JSON);
-//			
-//			return new ResponseEntity<>(taskJson.toString(), httpHeaders, HttpStatus.CREATED);
-//		
-//		} catch (DAOException de) {
-//			if (de.getCause() instanceof ConstraintViolationException) { // TODO steklsim is this enough?
-//				return new ResponseEntity<>(null, null, HttpStatus.BAD_REQUEST);
-//			} else {
-//				return new ResponseEntity<>(null, null, HttpStatus.INTERNAL_SERVER_ERROR);
-//			}
-//		} catch (JsonException je) {
-//			return new ResponseEntity<>(null, null, HttpStatus.BAD_REQUEST);
-//		}
+
     task.setId(id);
     Person testUser = personAdmin.getOsoba(ApiConstants.TEST_USER_ID);
     populateTask(task, testUser);
